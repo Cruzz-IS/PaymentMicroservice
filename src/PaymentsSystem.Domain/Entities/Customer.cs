8e3a252a25d4e885003c34691dc72b6a9b279c20
cruz-IS <edwarcruz2000@yahoo.com>
2026-04-29 23:56:18 -0600

    creacion de las entidades necesarias para el modelo de negocio

---
+++ src/PaymentsSystem.Domain/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Domain.Entities
+{
+    [Table("Customers")]
+    internal class Customer
+    {
+    }
+}
---
+++ src/PaymentsSystem.Domain/Entities/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Domain.Entities
+{
+    [Table("Users")]
+    internal class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 80 caracteres")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El email es requerido")]
+        [EmailAddress(ErrorMessage = "El formato del email no es válido")]
+        [StringLength(200)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+
+        [Phone(ErrorMessage = "El formato del teléfono no es válido")]
+        [StringLength(20)]
+        public string? PhoneNumber { get; set; }
+
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "El nombre de usuario debe tener entre 2 y 20 caracteres")]
+        public string? Username { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public bool EmailConfirmed { get; set; } = false;
+
+        public int FailedLoginAttempts { get; set; } = 0;
+
+        public DateTime? LockoutEnd { get; set; }
+
+        public DateTime? LastLoginDate { get; set; }
+
+        public DateTime? PasswordChangedDate { get; set; }
+
+    }
+}
