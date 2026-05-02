b98c90e6d642c8b5d2e5bb6bc8c279e8cd80314b
cruz-IS <edwarcruz2000@yahoo.com>
2026-05-01 23:58:28 -0600

    configuramos las restricciones a la tabla customers

---
+++ src/PaymentsSystem.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PaymentsSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Infrastructure.Persistence.Configurations
+{
+    // Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("Customers");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasDefaultValueSql("NEWSEQUENTIALID()");
+
+            builder.Property(c => c.FullName)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            //builder.HasMany(c => c.Orders)
+            //    .WithOne(o => o.Customer)
+            //    .HasForeignKey(o => o.CustomerId)
+            //    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
