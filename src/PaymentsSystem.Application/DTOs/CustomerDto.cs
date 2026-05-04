31da2b7f86ec0edb64baf0bb9c01a81735434c1d
cruz-IS <edwarcruz2000@yahoo.com>
2026-05-03 23:27:11 -0600

    interfaz y dto de los customers

---
+++ src/PaymentsSystem.Application/DTOs/CustomerDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Application.DTOs
+{
+    public record CustomerDto(
+    Guid Id,
+    string FullName,
+    string Email,
+    string? Phone,
+    bool IsActive,
+    DateTime CreatedAt
+);
+}
---
+++ src/PaymentsSystem.Application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using PaymentsSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Application.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Customer?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Customer>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task AddAsync(Customer customer, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Customer customer, CancellationToken cancellationToken = default);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
