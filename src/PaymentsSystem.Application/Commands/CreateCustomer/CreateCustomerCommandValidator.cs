599150b53160e5e2ccce24a3572b7ca76e0baa8b
cruz-IS <edwarcruz2000@yahoo.com>
2026-05-06 23:57:38 -0600

    creacion de deteccion de excepciones y validaciones

---
+++ src/PaymentsSystem.Application/Commands/CreateCustomer/CreateCustomerCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PaymentsSystem.Application.Commands.CreateCustomer; // Asegúrate de que el namespace es correcto y contiene CreateCustomerCommand
+    
+namespace PaymentsSystem.Application.Commands.CreateCustomer
+{
+    // Asegúrate de que la clase CreateCustomerCommand existe en este namespace
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty().WithMessage("El nombre completo es obligatorio.")
+                .MaximumLength(200).WithMessage("El nombre no puede superar 200 caracteres.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("El email es obligatorio.")
+                .EmailAddress().WithMessage("El formato del email no es válido.")
+                .MaximumLength(256).WithMessage("El email no puede superar 256 caracteres.");
+
+            RuleFor(x => x.Phone)
+                .MaximumLength(20).WithMessage("El teléfono no puede superar 20 caracteres.")
+                .When(x => x.Phone is not null);
+        }
+    }
+}
---
+++ src/PaymentsSystem.Domain/Exceptions/CustomerNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Domain.Exceptions
+{
+    public class CustomerNotFoundException : Exception
+    {
+        public CustomerNotFoundException(Guid customerId)
+            : base($"El cliente con ID {customerId} no fue encontrado.")
+        {
+        }
+
+        public CustomerNotFoundException(string email)
+            : base($"El cliente con email {email} no fue encontrado.")
+        {
+        }
+    }
+}
