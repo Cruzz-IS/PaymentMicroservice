d3dd95842378cfc65509d5297ee245c21a2d387b
cruz-IS <edwarcruz2000@yahoo.com>
2026-05-08 22:55:36 -0600

    creacion del controller y del middleware de autenticacion

---
+++ src/PaymentSystem.API/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PaymentsSystem.Application.Commands;
+using PaymentsSystem.Application.Queries.GetCustomerById;
+using PaymentsSystem.Application.Commands.CreateCustomer;
+using MediatR;
+
+namespace PaymentMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CustomersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [AllowAnonymous] 
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> Create(
+            [FromBody] CreateCustomerCommand command,
+            CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(
+            Guid id,
+            CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new GetCustomerByIdQuery(id), cancellationToken);
+            return Ok(result);
+        }
+    }
+}
---
+++ src/PaymentSystem.API/Middlewares/JwtMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace PaymentMicroservice.Middlewares
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Verifica si usuario cuenta con el token del header Authorization
+            var result = await context.AuthenticateAsync();
+
+            if (result.Succeeded)
+                context.User = result.Principal;
+
+            await _next(context);
+        }
+    }
+}
