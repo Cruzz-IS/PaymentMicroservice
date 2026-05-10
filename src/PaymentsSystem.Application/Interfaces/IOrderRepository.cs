b0a232e20d50fb209ca04fc919ca09202a11a3d0
cruz-IS <edwarcruz2000@yahoo.com>
2026-05-10 00:07:55 -0600

    consultas de las ordenes a la base de datos

---
+++ src/PaymentsSystem.Application/Interfaces/IOrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using PaymentsSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Application.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<Order?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<Order?> GetByIdWithDetailsAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<Order>> GetByCustomerIdAsync(
+            Guid customerId,
+            CancellationToken cancellationToken = default);
+
+        Task AddAsync(Order order, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Order order, CancellationToken cancellationToken = default);
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
