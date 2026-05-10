7537ef1bf986b767d05b892e7b47be384e920008
cruz-IS <edwarcruz2000@yahoo.com>
2026-05-10 00:10:50 -0600

    command para las ordenes

---
+++ src/PaymentsSystem.Application/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using PaymentsSystem.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Application.Commands.CreateOrder
+{
+    public record OrderItemRequest(
+    string ProductName,
+    int Quantity,
+    decimal UnitPrice
+);
+
+    public record CreateOrderCommand(
+        Guid CustomerId,
+        string Currency,
+        List<OrderItemRequest> Items
+    ) : IRequest<OrderDto>;
+}
