943d938ef4e95114ae69dd85d1cb80130fc2716b
cruz-IS <edwarcruz2000@yahoo.com>
2026-05-09 23:48:31 -0600

    creacion de los dto de las ordenes

---
+++ src/PaymentsSystem.Application/DTOs/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Application.DTOs
+{
+    public record OrderDto(
+    Guid Id,
+    Guid CustomerId,
+    string CustomerName,
+    string Status,
+    decimal TotalAmount,
+    string Currency,
+    DateTime CreatedAt,
+    IReadOnlyCollection<OrderItemDto> Items
+);
+}
---
+++ src/PaymentsSystem.Application/DTOs/OrderItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsSystem.Application.DTOs
+{
+    public record OrderItemDto(
+    Guid Id,
+    string ProductName,
+    int Quantity,
+    decimal UnitPrice,
+    decimal Subtotal
+);
+}
